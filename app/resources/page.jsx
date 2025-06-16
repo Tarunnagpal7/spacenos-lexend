311faf0ec76723966e844ab29ec7307668db740a
tarunnagpal7 <tarunnagpal192004@gmail.com>
2025-06-16 14:10:12 +0530

    update header of resources

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -1,4 +1,4 @@
-import Header2 from "@/components/headers/Header2";
+import Header5 from "@/components/headers/Header5";
 
 import Footer5 from "@/components/footers/Footer5";
 import ToolHero from "@/components/innerpages/ToolHero";
@@ -13,7 +13,7 @@ export default function BlogPage1() {
   return (
     <>
       <div className="page-wrapper uni-body panel bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-200 overflow-x-hidden bp-xs bp-sm bp-md bp-lg bp-xl bp-xxl dom-ready">
-        <Header2 />
+        <Header5 />
         <div id="wrapper" className="wrap">
         <ResourceHero />
         <LeadMagnets />
