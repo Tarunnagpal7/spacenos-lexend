1250cd3f868e0a6c3002f25637f0dcf261569d49
tarunnagpal7 <tarunnagpal192004@gmail.com>
2025-06-24 15:52:41 +0530

    Deleted headers , footers and homes pages for optimzation

--- app/(blogs)/blog-author/[author]/page.jsx
+++ app/(blogs)/blog-author/[author]/page.jsx
@@ -1,6 +1,6 @@
-import Header2 from "@/components/headers/Header2";
+import Header5 from "@/components/headers/Header5";
 
-import Footer2 from "@/components/footers/Footer2";
+import Footer5 from "@/components/footers/Footer5";
 import Newsletter from "@/components/blog/Newsletter";
 import BreadCumb from "@/components/blog/BreadCumb";
 import Blogs4 from "@/components/blog/Blogs4";
@@ -16,13 +16,13 @@ export default async function BlogAuthorPage(props) {
   return (
     <>
       <div className="page-wrapper uni-body panel bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-200 overflow-x-hidden bp-xs bp-sm bp-md bp-lg bp-xl bp-xxl dom-ready">
-        <Header2 />
+        <Header5 />
         <div id="wrapper" className="wrap">
           <BreadCumb category={author} />
           <Blogs4 author={author} />
           <Newsletter />
         </div>
-        <Footer2 />
+        <Footer5 />
       </div>
     </>
   );
