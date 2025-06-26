0ff2c0dc29e71c6e6b9281819bc399ab86ce5859
tarunnagpal7 <tarunnagpal192004@gmail.com>
2025-06-26 14:33:26 +0530

    Config Microsoft Clarity

--- app/layout.js
+++ app/layout.js
@@ -67,8 +67,8 @@ export default function RootLayout({ children }) {
               delay: animeSettings.delay
                 ? animeSettings.delay.includes(",")
                   ? anime.stagger(animeSettings.delay.split(",")[0] / 1, {
-                      start: animeSettings.delay.split(",")[1] / 1,
-                    })
+                    start: animeSettings.delay.split(",")[1] / 1,
+                  })
                   : animeSettings.delay / 1
                 : 0,
             });
@@ -164,19 +164,20 @@ export default function RootLayout({ children }) {
       />
 
       {/* Microsoft Clarity Script */}
-      {/* <Script
+      <Script
         id="microsoft-clarity"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "YOUR_CLARITY_PROJECT_ID");
-          `,
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "${CLARITY_ID}");
+    `,
         }}
-      /> */}
+      />
+
 
       <body>
         <Context>
